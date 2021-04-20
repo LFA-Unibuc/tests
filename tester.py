ddb7df05020b1351644ab0b1517abd719f77e8b3
ilikehaskell <ouatubogdanioan@gmail.com>
2021-04-20 14:10:31 +0300

    folder structure

---
+++ tester.py
@@ -0,0 +1,8 @@
+import sys, os
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+from dfa.dfa import DFA
+# from nda.nfa import NFA
+
+dfa = DFA('automaton.cfg')
+test(dfa.accepts_string('string_1.in') == parse('string_1.out'))
